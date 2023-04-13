1f8d482d1880e1cedfc5115776940c274fd0c772
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-13 20:14:06 -0300

    ai900

--- MachineLearning/mleaning.tf
+++ MachineLearning/mleaning.tf
@@ -5,40 +5,40 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+  name     = "cognitiveia900"
+  location = "East US"
 }
 
-resource "azurerm_application_insights" "example" {
-  name                = "workspace-example-ai"
+resource "azurerm_application_insights" "cognitiveia900" {
+  name                = "cognitiveia900-ai"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   application_type    = "web"
 }
 
-resource "azurerm_key_vault" "example" {
-  name                = "workspaceexamplekeyvault"
+resource "azurerm_key_vault" "cognitiveia900" {
+  name                = "cognitiveia900keyvault"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "premium"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "workspacestorageaccount"
+resource "azurerm_storage_account" "cognitiveia900" {
+  name                     = "cognitiveia900"
   location                 = azurerm_resource_group.example.location
   resource_group_name      = azurerm_resource_group.example.name
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
 
-resource "azurerm_machine_learning_workspace" "example" {
-  name                    = "example-workspace"
+resource "azurerm_machine_learning_workspace" "cognitiveia900" {
+  name                    = "cognitiveia900-workspace"
   location                = azurerm_resource_group.example.location
   resource_group_name     = azurerm_resource_group.example.name
-  application_insights_id = azurerm_application_insights.example.id
-  key_vault_id            = azurerm_key_vault.example.id
-  storage_account_id      = azurerm_storage_account.example.id
+  application_insights_id = azurerm_application_insights.cognitiveia900.id
+  key_vault_id            = azurerm_key_vault.cognitiveia900.id
+  storage_account_id      = azurerm_storage_account.cognitiveia900.id
 
   identity {
     type = "SystemAssigned"
