5ad84666c877a0ec6385d43c77a8e7719a2fc1a4
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-13 19:40:59 -0300

    ai900

--- MachineLearning/mleaning.tf
+++ MachineLearning/mleaning.tf
@@ -1,41 +1,24 @@
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_resource_group" "example" {
-  name     = "example-rg"
-  location = "west europe"
-  tags = {
-    "stage" = "example"
-  }
+provider "azurerm" {
+  features {}
 }
-
-resource "azurerm_application_insights" "example" {
-  name                = "example-ai"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  application_type    = "web"
-}
-
-resource "azurerm_key_vault" "example" {
-  name                = "example-kv"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-
-  sku_name = "standard"
-
-  purge_protection_enabled = true
-}
-
 
 resource "azurerm_machine_learning_workspace" "example" {
-  name                    = "example-mlw"
-  location                = azurerm_resource_group.example.location
-  resource_group_name     = azurerm_resource_group.example.name
-  application_insights_id = azurerm_application_insights.example.id
-  key_vault_id            = azurerm_key_vault.example.id
-  storage_account_id      = azurerm_storage_account.example.id
+  name                = "example-ml"
+  location            = "eastus"
+  resource_group_name = "example-resource-group"
+  friendly_name       = "Example Machine Learning Workspace"
+  storage_account_id  = azurerm_storage_account.example.id
+}
 
-  identity {
-    type = "SystemAssigned"
-  }
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorage"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resource-group"
+  location = "eastus"
 }
--- MachineLearning/storage.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_storage_account" "example" {
-  name                     = "examplesa"
-  location                 = azurerm_resource_group.example.location
-  resource_group_name      = azurerm_resource_group.example.name
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
