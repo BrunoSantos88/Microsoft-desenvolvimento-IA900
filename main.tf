6dda292f6f1a0d7411e57548acec3bc15ccff8ca
Bruno Santos F. Da Silva <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-14 08:51:30 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # SELECIONAR MODULO ANTES DE INICIAR TERRAFORM
 
-LAB CognitiveServices
-#module "Cognitive" {
-#source = "./Cognitive"   
-#}
+#LAB CognitiveServices
+module "Cognitive" {
+source = "./Cognitive"   
+}
 
 #LAB Machine Learning
 module "MachineLearning" {
