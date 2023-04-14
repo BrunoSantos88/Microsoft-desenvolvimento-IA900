95de49a77d080bb6b57b923c7b731e5080af3e41
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-14 16:50:24 -0300

    ia900

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
 # SELECIONAR MODULO ANTES DE INICIAR TERRAFORM
 
-#LAB CognitiveServices
-#module "Cognitive" {
-#source = "./Cognitive"   
-#}
-
 #LAB Machine Learning
-module "MachineLearning" {
-  source = "./machinelearning"   
+module "LabPraticoAI900" {
+  source = "./LabPraticoAI900"   
 }
